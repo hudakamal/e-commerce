03d5f1f09b5e812515d49af4bd835ee116dcfd6d
hudakamal <kamalhuda4@gmail.com>
2023-05-30 09:22:33 +0800

    test 1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,15 @@
 @extends('layouts.menu')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('Lorem ipsum dolor sit amit,consectetur eiusmdd tempor incididunt ut labore et dolore magna Lorem ipsum dolor sit
-
-amit,consectetur eiusmdd tempor incididunt ut labore et dolore magna') }}
-                </div>
-            </div>
+<div class="container-fluid p-0 position-relative">
+    <img src="{{ asset('image/home.png') }}" class="img-fluid w-100" alt="Background Image" style="height: 350px;">
+    <div class="position-absolute top-50 start-50 translate-middle">
+        <div class="text-left">
+            <h2 style="color:#FF6666;margin-bottom: 10px;">Sales 20% Off</h2>
+            <h2 style="color:#4C4C6D;font-weight: bold;margin-bottom: 20px;">On Everything</h2>
+            <p style="line-height: 1.5;">Massive Markdowns: Don't Miss Our Biggest Sale of the Year</p>
         </div>
+        <button class="btn" style="background-color: #FF6666;">Shop Now</button>
     </div>
 </div>
 @endsection
